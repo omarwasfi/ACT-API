c3b00a586623ec51aa0d31ab9aad4a0ee0344c2c
Omar Wasfi <alprincewasfi@gmail.com>
2021-03-30 20:10:02 +0200

    Systems overview DEMO

---
+++ Backend/ACT/ACT/DataModels/PK1_PSTG_DETAIL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACT.DataModels
+{
+    public class PK1_PSTG_DETAIL
+    {
+        public int PSTG_HDR_ID { get; set; }
+
+        public int LINE_NUM { get; set; }
+
+        public int UPDATE_COUNT { get; set; }
+
+        public string LAST_CHANGE_USER_ID { get; set; }
+
+        public DateTime LAST_CHANGE_DATETIME { get; set; }
+
+        public string ACNT_CODE { get; set; }
+
+        public int PERD { get; set; }
+
+        public DateTime TXN_DATETIME { get; set; }
+
+        public int JNL_NUM { get; set; }
+
+    }
+}
